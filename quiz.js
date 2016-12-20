6d71371d88edeba4babf6a3b771051a335b74b0c
sirwilliamiv <wjconnolly2@gmail.com>
2016-12-20 10:10:53 -0600

    set function to  catch value of idtypingtexthere and moved inside events function

--- quiz.js
+++ quiz.js
@@ -1,86 +1,35 @@
-// THE COMPLETE API SET: EXPLORATORY
 
-// Key: g48g6fa3bmuztnxvdvcyasdw
-
-// ApplicationCar Research APP Key: g48g6fa3bmuztnxvdvcyasdw Secret: SJm3bcNMHw97XcmgFW9Fzxeu Status: active Created: 3 seconds ago
-// Key Rate Limits
-// 5 Calls per second
-// 25  Calls per day
-
-console.log("hey javascript")
-
-
-
-
-// I will hold the json file native js object CHECKED
+var parent  = ""
+var thisIsTheEditText ="";
 var inventory = [];
 var selectTheseCards;
 loadInventory();
 
-// function takes in element and adds class  clicked-card, sets focus to text input
-
+// function takes in element and adds class  clicked-card, sets focus to text input/resets text field to ""
 function changing(element, color) {
-    element.classList.add("clicked-card")
-    element.classList.add("clicked-card")
-    document.getElementById("typingTextHere").focus()
+          element.classList.add("clicked-card")
+          element.classList.add("clicked-card")
+          document.getElementById("typingTextHere").focus()
+          document.getElementById('typingTextHere').value = ""
 
-       whenToStartTyping(element)
 
 }
 
-
-      function resetStyleOnClick (clickedElement) {
+// selects all cards and removes styling class
+function resetStyleOnClick (clickedElement) {
         for(var i =0; i < selectTheseCards.length;  i++) {
           selectTheseCards[i].classList.remove("clicked-card")
         }
-      }
-
-// changing the text in  description function
-//
- // function  letMeChangeTheDescriptionForYou(newText) {
-
-
- //      document.querySelector('.card-text').innerHTML = ""
- //      document.querySelector('.card-text').innerHTML += newText
-
-
- // }
-var parent  = ""
-var thisIsTheEditText ="";
-// this function is only called inside click event to prevent typing editing when no element selected
-
-function whenToStartTyping(parent) {
-// resetStyleOnClick()
-document.getElementById("typingTextHere").addEventListener('keyup', function editTheDescription(event){
- parent.querySelector('.card-text').innerHTML += ""
-
-            if(parent === "") {
-              return
-            }
-         else if (event.code === "Enter" ) {
-              return false;
-          }
-        else if (event.code === "Backspace" ) {
-             thisIsTheEditText = thisIsTheEditText.substring(0, thisIsTheEditText.length -1)
-
-
-            }
-            else {
-
-                    thisIsTheEditText = event.key;
-                    // console.log(elementINeed)
-                // parent.querySelector('.card-text').innerHTML = "" NEED TO RESET HTML TEXT
-                parent.querySelector('.card-text').innerHTML += ""
-                parent.querySelector('.card-text').innerHTML += thisIsTheEditText
-
-            }
- })
-    }
+}
 
 
 
-// THis will put content into html once  html is built up through loop
-//
+
+
+
+
+// This will put content into html once  html is built up through loop
+
 
 function populatePage (changethisname) {
   var createTheCard="";
@@ -107,7 +56,6 @@ function populatePage (changethisname) {
 }
 
 // =================================// this selects the card and changes the color====================================================
-
 
 function changeBorderAndBackground(event) {
             var clickedElement = event.target
@@ -142,16 +90,38 @@ for(var i =0; i < selectTheseCards.length;  i++) {
 
       selectTheseCards[i].addEventListener('click',changeBorderAndBackground)
     }
-  }
+
+// typing into description event
+
+document.getElementById("typingTextHere").addEventListener('keyup', function editTheDescription(event){
+
+
+            if(parent === "") {
+              return
+            }
+         else if (event.code === "Enter" ) {
+              return false;
+          }
+
+            else {
+
+                    parent.querySelector('.card-text').innerHTML = ""
+                    thisIsTheEditText = event.key;
+                    parent.querySelector('.card-text').innerHTML += ""
+                    parent.querySelector('.card-text').innerHTML += document.getElementById('typingTextHere').value
+
+            }
+ })
+}
 
 function sendToTheDom(builtHTMLFromPopulatePage) {
   // console.log(builtHTMLFromPopulatePage)
-document.getElementById('putTheCardHere').innerHTML = builtHTMLFromPopulatePage
+        document.getElementById('putTheCardHere').innerHTML = builtHTMLFromPopulatePage
 
 // load up array of  created divs
-    selectTheseCards = document.querySelectorAll('.card')
+          selectTheseCards = document.querySelectorAll('.card')
     // console.log(selectTheseCards)
-    loopTheCards(selectTheseCards)
+          loopTheCards(selectTheseCards)
 
     }
 
